1c1e9cec765394a9660bd17f3b2f3f136806fb9e
Felipe <machadofelipe2016@outlook.com>
2022-04-29 14:02:30 -0300

    c1-practice more little bit about filter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,11 +4,17 @@ import './App.css'
 
 const NameEmployer = ['programer','Felipe Martins Machado','Programmer18', 'Betina', 'Joao']
 
+const FilterNumberId = [21,3,232,4324,123,43234,3434,44,10]
+
 function App() {
   const [busca, setbusca] = useState('')
   const Resumo =  busca.toLowerCase()
-   const Union = NameEmployer.filter((call) => call.toLowerCase().includes(Resumo))
+  const Union = NameEmployer.filter((call) => call.toLowerCase().includes(Resumo))
  
+  const [caught, setcaught] = useState('')
+  const Together = caught.toLowerCase()
+  const Equaly = FilterNumberId.filter((eq) => eq.toLowerCase().includes(Together))
+
   return (
     <div className="App">
 
@@ -17,7 +23,13 @@ function App() {
         {Union.map((call) =>(
           <li key={call}>{call}</li>
         ))}
+      </ul>
 
+      <input type='text' onChange={(ev => setcaught(ev.target.value))} />
+      <ul> { Equaly.map((eq) => (
+        <li key={eq}>{eq}</li>
+      ))       
+      }
       </ul>
      
     </div>
