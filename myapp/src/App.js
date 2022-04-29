c684da58d357141377c13c9a5c1ead4b3b020447
Felipe <machadofelipe2016@outlook.com>
2022-04-29 14:10:10 -0300

    c2-adding more code to practice

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,16 +3,15 @@ import { useState } from 'react';
 import './App.css'
 
 const NameEmployer = ['programer','Felipe Martins Machado','Programmer18', 'Betina', 'Joao']
-
-const FilterNumberId = [21,3,232,4324,123,43234,3434,44,10]
+const FilterNumberId = ["21","3","232","4324",'123','43234','3434','44','10']
 
 function App() {
   const [busca, setbusca] = useState('')
+  const [caught, setcaught] = useState('')
   const Resumo =  busca.toLowerCase()
   const Union = NameEmployer.filter((call) => call.toLowerCase().includes(Resumo))
  
-  const [caught, setcaught] = useState('')
-  const Together = caught.toLowerCase()
+  const Together = caught
   const Equaly = FilterNumberId.filter((eq) => eq.toLowerCase().includes(Together))
 
   return (
@@ -25,8 +24,8 @@ function App() {
         ))}
       </ul>
 
-      <input type='text' onChange={(ev => setcaught(ev.target.value))} />
-      <ul> { Equaly.map((eq) => (
+      <input type='text' onChange={(evt => setcaught(evt.target.value))} />
+      <ul> {Equaly.map((eq) => (
         <li key={eq}>{eq}</li>
       ))       
       }
